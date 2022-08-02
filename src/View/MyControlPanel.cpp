deeaea2bfe0cf4b456a73ab80b8f32dc569295da
baldojerome <99471537+baldojerome@users.noreply.github.com>
2022-08-02 23:20:28 +0200

    Create MyControlPanel.cpp

---
+++ src/View/MyControlPanel.cpp
@@ -0,0 +1,81 @@
+using namespace std ;
+#include <iostream>
+#include <cstdlib>
+
+#include "constants.hpp"
+#include "ID.hpp"
+#include "MyControlPanel.hpp"
+#include "MyFrame.hpp"
+
+
+//************************************************************************
+//************************************************************************
+// MyControlePanel class (where controls are displayed)
+//************************************************************************
+//************************************************************************
+
+//------------------------------------------------------------------------
+MyControlPanel::MyControlPanel(wxWindow *parent) : wxPanel(parent)
+//------------------------------------------------------------------------
+// In this constructor, create the controls and associate each of them (bind) a method
+{
+	int w, h, y ;
+	GetParent()->GetSize(&w,&h) ;
+	SetSize(wxRect(wxPoint(0,0), wxPoint(WIDGET_PANEL_WIDTH, h))) ;
+	SetBackgroundColour(*wxLIGHT_GREY) ;
+
+	y = WIDGET_Y0 ;
+	m_radioNavi = new wxRadioButton(this, ID_RADIONAVIG, wxT("Navigation"), wxPoint(10, y)) ;
+	Bind(wxEVT_RADIOBUTTON, &MyControlPanel::GetRadioB, this, ID_RADIONAVIG) ;
+
+	y += 40;
+	m_radioTrait = new wxRadioButton(this, ID_RADIOTRAIT, wxT("Trait"), wxPoint(10, y));
+	Bind(wxEVT_RADIOBUTTON, &MyControlPanel::GetRadioB, this, ID_RADIOTRAIT);
+
+	y += 30;
+	m_radioRectangle = new wxRadioButton(this, ID_RADIORECT, wxT("Rectangle"), wxPoint(10, y));
+	Bind(wxEVT_RADIOBUTTON, &MyControlPanel::GetRadioB, this, ID_RADIORECT);
+
+	y += 30;
+	m_radioCercle = new wxRadioButton(this, ID_RADIOCERCL, wxT("Cercle"), wxPoint(10, y));
+	Bind(wxEVT_RADIOBUTTON, &MyControlPanel::GetRadioB, this, ID_RADIOCERCL);
+
+}
+/*
+méthode vérifiant tous les radiobuttons. 
+Ne pouvant qu'avoir un seul bouton sur "true" la méthode vérifie 
+et sélectionne l'état dans lequel doit être MyDrawingPanel
+RAPPEL:
+ETAT 0 = NAVIGATION
+ETAT 1 = TRAIT
+ETA 2 = RECTANGLE 
+ETAT 3 = CERCLE 
+la methode modifie l'attribut etat dans MyDrawingPanel.
+*/
+//------------------------------------------------------------------------
+void MyControlPanel::GetRadioB(wxCommandEvent &event)
+//------------------------------------------------------------------------
+{
+	int etat;
+	
+	if(m_radioTrait->GetValue() )
+	{
+		
+		etat = 1;
+	}
+	else if(m_radioRectangle->GetValue() )
+	{
+		etat = 2;
+	}
+	else if(m_radioCercle->GetValue() )
+	{
+		etat = 3;
+	}
+	else
+	{
+		etat = 0;
+	}
+	
+	MyFrame* frame = (MyFrame*)GetParent() ;
+	frame->GetDrawingPanel()->SetEtat(etat) ;
+}
