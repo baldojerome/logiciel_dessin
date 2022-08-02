a8e720e7b9fd3df92a845b511bbb5ce008c22d76
baldojerome <99471537+baldojerome@users.noreply.github.com>
2022-08-02 23:19:53 +0200

    Create Forme.h

---
+++ src/Modele/Forme.h
@@ -0,0 +1,29 @@
+#ifndef __FORME_H__
+#define __FORME_H__
+
+#include "../View/include.hpp"
+#include <string>
+#include <iostream>
+
+class Forme
+{
+public:
+    // Construction/Destruction
+    Forme() = default;
+    Forme(const Forme& other);          // Construction par recopie
+    virtual ~Forme() = default;
+
+    // Operator d'affectation
+    Forme& operator=(const Forme& other);
+
+    //Méthode d'affichage des infos et de dessin sur le drawpanel
+    virtual void Display() const = 0 ;
+    virtual void Draw(wxDC* dc) const = 0;
+    /*virtual float Surface() const { return 0.0f; }
+    virtual float Perimeter() const { return 0.0f; }*/
+
+//protected:
+
+};
+
+#endif 
