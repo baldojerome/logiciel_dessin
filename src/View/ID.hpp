04550759744570443e77ed911d574733c1364bee
baldojerome <99471537+baldojerome@users.noreply.github.com>
2022-08-02 23:20:15 +0200

    Create ID.hpp

---
+++ src/View/ID.hpp
@@ -0,0 +1,16 @@
+//------------------------------------------------------------------------
+// Some enums for widgets
+//------------------------------------------------------------------------
+enum
+{
+	ID_QUIT = 1,
+	ID_ABOUT,
+	ID_LOAD,
+	ID_SAVE,
+	ID_RADIONAVIG, //permet de clicker sans créer de dessin 
+	ID_RADIOTRAIT, //trait
+	ID_RADIORECT, //rectangle
+	ID_RADIOCERCL, //cercle
+	ID_SLIDER1,
+	ID_CHECKBOX1
+};
