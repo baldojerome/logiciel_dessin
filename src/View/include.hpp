d4f6df4417611e56e964f40c9f942669d504c22e
baldojerome <99471537+baldojerome@users.noreply.github.com>
2022-08-02 23:20:17 +0200

    Create include.hpp

---
+++ src/View/include.hpp
@@ -0,0 +1,12 @@
+#include "wx/wxprec.h"
+
+#ifndef WX_PRECOMP
+    #include "wx/wx.h"
+#endif
+
+#include<wx/filedlg.h>
+#include <wx/image.h>
+#include <wx/file.h>
+#include <wx/bitmap.h>
+#include <wx/dcsvg.h>
+#include <wx/brush.h>
